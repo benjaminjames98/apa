a3db54697b0d5f14fabc9916c7182b8c09f0fc2f
Ben <benjamin.james98@gmail.com>
2022-01-26 12:23:48 +1100

    implemented Express.js for http routing

--- node/src/core.js
+++ node/src/core.js
@@ -1,6 +1,11 @@
-const http = require("http");
 const dotenv = require("dotenv");
 dotenv.config();
+
+const express = require('express');
+const cors = require('cors');
+const app = express();
+const port = 8080;
+app.use(cors());
 
 //const notion = require('./notion');
 const events = require('./events');
@@ -8,32 +13,22 @@ const events1 = require('./events');
 const calendar = require('./google/calendar');
 const jwt_client = require('./google/jwt_client');
 
+app.get('/node/', (request, result) => {
+  calendar.get_events_from_week(new Date())
+    .then(events.load_events)
+    .then(() => result.json(events.category_info))
+    .catch(e => result.json(e));
+});
 
 calendar.init(jwt_client)
   .then(start_http_server);
 
 function start_http_server() {
-  http.createServer(http_request_listener).listen(8080);
+  app.listen(port, () => {
+    console.log(`Example app listening on port ${port}`);
+  });
 }
 
-function http_request_listener(request, result) {
-  // TODO Allow functions based on input (i.e. get events pre-parsing or pre-duration_summing)
 
-  calendar.get_events_from_week(new Date())
-    .then(events.load_events)
-    .then(() => return_http_result(result, events.category_info))
-    .catch(e => return_http_result(result, e));
-
-  // notion.get_pages_from_week(process.env.NOTION_DATABASE_ID)
-  //   .then(r => return_http_result(result, r))
-  //   .catch(e => return_http_result(result, e));
-
-}
-
-function return_http_result(result, obj) {
-  result.writeHead(200, {'Content-Type': 'application/json'});
-  result.write(JSON.stringify(obj));
-  result.end();
-}
 
 
